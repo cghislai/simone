62848116fcb0adbe5464b79c925f150aa0d4254d
cghislai <charlyghislain@gmail.com>
2017-09-02 01:40:49 +0200

    switch config when following task container links

--- src/app/docker/unique-id/unique-id.component.ts
+++ src/app/docker/unique-id/unique-id.component.ts
@@ -1,4 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {DockerClientConfigService} from '../services/docker-client.service';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
   selector: 'app-unique-id',
@@ -10,20 +13,62 @@ export class UniqueIdComponent implements OnInit {
   @Input()
   id: string;
   @Input()
-  routerLink: any[];
+  type: string;
+  @Input()
+  label: string;
+  @Input()
+  nodeId: string;
+
   @Input()
   noTrim: boolean;
   @Input()
   trimWidthEm: number = 6;
 
-  constructor() {
+  path: string;
+
+  constructor(private router: Router,
+              private configService: DockerClientConfigService) {
   }
 
   ngOnInit() {
+    this.path = `${this.type}s`;
   }
 
   getMaxWidthCss(): string {
     return this.noTrim ? null : `${this.trimWidthEm}em`;
   }
 
+  onClick() {
+    this.checkNodeConfig()
+      .subscribe(null, null, () => {
+        this.router.navigate(['/docker', this.path, this.id]);
+      });
+  }
+
+  private checkNodeConfig(): Observable<any> {
+    if (this.nodeId == null) {
+      return Observable.empty();
+    }
+    let config = this.configService.getActiveConfigNow();
+    if (config == null) {
+      return Observable.empty();
+    }
+    let serverInfo = config.serverInfo;
+    if (serverInfo == null) {
+      return Observable.empty();
+    }
+    let serverNodeId = serverInfo.swarmNodeId;
+    if (serverNodeId == null || serverNodeId == this.nodeId) {
+      return Observable.empty();
+    }
+
+    return this.configService.getConfigList()
+      .take(1)
+      .map(list => list.filter(config => config.serverInfo != null)
+        .find(config => config.serverInfo.swarmNodeId == this.nodeId))
+      .filter(config => config != null)
+      .do(config => {
+        this.configService.setActiveConfig(config);
+      });
+  }
 }
